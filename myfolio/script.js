e84344bd3209ee68a9dce726e9589dee59408cb2
Aditi3112 <Aditiangotra3112@gmail.com>
2024-07-24 12:28:27 +0530

    Add new file

---
+++ myfolio/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "Backend Developer", "Mern Stack Developer"],
+    typeSpeed : 10,
+    backSpeed : 150,
+    backDelay : 1000,
+    loop : true
+});
+
